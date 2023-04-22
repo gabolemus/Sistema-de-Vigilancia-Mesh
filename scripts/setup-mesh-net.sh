09470d9a3de260013928588af95391d3a5e73afe
Gabriel Lemus <glemus.stuart@gmail.com>
2023-04-22 11:50:32 -0600

    Script para configurar la red mesh

---
+++ scripts/setup-mesh-net.sh
@@ -0,0 +1,21 @@
+# Interface name
+IFACE=wlp2s0
+
+# Stop DHCP and WPA services
+sudo systemctl stop wpa_supplicant
+sudo systemctl mask wpa_supplicant
+# sudo update-rc.d dhcpcd disable
+
+# Setup the ad-hoc network
+sudo ip link set $IFACE down
+sudo iw $IFACE set type ibss
+sudo ifconfig $IFACE mtu 1532
+sudo iwconfig $IFACE channel 6
+sudo ip link set $IFACE up
+sudo iw $IFACE ibss join sistemas-inalambricos 2432
+
+# BATMAN-ADV configuration
+sudo modprobe batman-adv
+sudo batctl if add $IFACE
+sudo ip link set bat0 up
+sudo ifconfig bat0
