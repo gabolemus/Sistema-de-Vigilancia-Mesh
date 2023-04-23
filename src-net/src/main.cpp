9b0d2cb7f13aac785dd85ae0618b40d2e9499a38
Gabriel Lemus <glemus.stuart@gmail.com>
2023-04-22 21:25:39 -0600

    Refactorizar código con la clase MeshNode

--- src-net/src/main.cpp
+++ src-net/src/main.cpp
@@ -1,29 +1,38 @@
+#include <arpa/inet.h>
 #include <csignal>
-#include <iostream>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "../include/MeshNode.hpp"
+
+// Constantes globales
+MeshNode meshNode;
 
 void signalInterruptionHandler(int signum) {
-  // Ejecutar el script para detener la red mesh
-  std::cout << "Deteniendo red mesh..." << std::endl;
-  system("./scripts/stop-mesh-net.sh");
-  std::cout << "Red mesh terminada." << std::endl;
+  // Imprimir caracter de borrado de línea
+  printf("\n\033[1A\033[2K");
+
+  // Detener el nodo en la red mesh
+  meshNode.stopMeshNode();
 
   exit(signum == SIGINT ? 0 : 1);
 }
 
 int main() {
-  // Registrar el manejador de señales
+  // Registrar el manejador de la señal SIGINT para detener la red mesh
   signal(SIGINT, signalInterruptionHandler);
 
-  // Ejecutar el script de configuración de la red mesh
-  std::cout << "Estableciendo red mesh..." << std::endl;
-  system("./scripts/setup-mesh-net.sh");
-  std::cout << "Red mesh establecida." << std::endl;
+  // Iniciar el nodo en la red mesh
+  meshNode = MeshNode();
+  meshNode.startMeshNode();
 
   // Esperar hasta que se presione Ctrl+C
   while (true) {
-    std::cout << "Presione Ctrl+C cuando desee detener la red mesh." << std::endl;
+    printf("Presione Ctrl+C para detener la red mesh.\n");
     sleep(5);
   }
 
