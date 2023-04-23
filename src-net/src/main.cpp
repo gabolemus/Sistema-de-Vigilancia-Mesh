67f96b98d35a52537e41fdb9baea1078eb314eed
Gabriel Lemus <glemus.stuart@gmail.com>
2023-04-23 13:32:04 -0600

    Actualizar main

--- src-net/src/main.cpp
+++ src-net/src/main.cpp
@@ -1,6 +1,15 @@
 #include <csignal>
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
 
 #include "../include/MeshNode.hpp"
+
+using namespace std;
 
 // Constantes globales
 MeshNode meshNode;
@@ -19,16 +28,29 @@ int main() {
   // Registrar el manejador de la señal SIGINT para detener la red mesh
   signal(SIGINT, signalInterruptionHandler);
 
-  // Iniciar el nodo en la red mesh
-  meshNode = MeshNode();
-  meshNode.startMeshNode();
-  meshNode.sendMessage();
+  int port = 8080;
+  string group = "ff02::1";
+  string message = "Hola, desde el nodo en la red mesh!";
+  int sockfd = socket(AF_INET6, SOCK_DGRAM, 0); // Crear el socket UDP IPv6
+  struct sockaddr_in6 addr;
 
-  // Esperar hasta que se presione Ctrl+C
-  while (true) {
-    meshNode.receiveMessage();
-    sleep(5);
-  }
+  memset(&addr, 0, sizeof(addr));
+  addr.sin6_family = AF_INET6;
+  addr.sin6_port = htons(port);
+  inet_pton(AF_INET6, group.c_str(), &addr.sin6_addr);
+
+  // Enviar mensaje a todos los nodos en la red mesh
+  printf("Enviando mensaje a todos los nodos en la red mesh...\n");
+  sendto(sockfd, message.c_str(), message.length(), 0, (struct sockaddr *)&addr, sizeof(addr));
+  printf("Mensaje enviado.\n");
+
+  // Esperar hasta recibir un mensaje de otro nodo en la red mesh
+  char buffer[1024];
+  struct sockaddr_in6 src_addr;
+  socklen_t src_addr_len = sizeof(src_addr);
+  recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&src_addr, &src_addr_len);
+
+  printf("Mensaje recibido: %s", buffer);
 
   return 0;
 }
