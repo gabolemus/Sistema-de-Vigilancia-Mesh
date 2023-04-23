fc7ba01bbb0bd5b3fb694480bbbef2c022fcde3a
Gabriel Lemus <glemus.stuart@gmail.com>
2023-04-22 21:45:33 -0600

    Agregar métodos del nodo

--- src-net/include/MeshNode.hpp
+++ src-net/include/MeshNode.hpp
@@ -9,6 +9,8 @@ class MeshNode
 private:
   int port;
   string group;
+  int sockfd;
+  string message;
 
 public:
   /** @brief Inicializar los atributos de la clase */
@@ -17,6 +19,18 @@ public:
   /** @brief Ejecutar script para configurar el nodo en la red mesh */
   void startMeshNode();
 
+  /** @brief Configurar dirección IPv6 y puerto del socket */
+  void configureSocket();
+
+  /** @brief Enviar mensaje a todos los nodos en la red mesh */
+  void sendMessage();
+
+  /** @brief Recibir mensaje de todos los nodos en la red mesh */
+  void receiveMessage();
+
   /** @brief Ejecutar script para detener el nodo en la red mesh */
   void stopMeshNode();
+
+  /** @brief Método destructor */
+  ~MeshNode();
 };
--- src-net/src/MeshNode.cpp
+++ src-net/src/MeshNode.cpp
@@ -1,11 +1,23 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 #include <iostream>
 
 #include "../include/MeshNode.hpp"
+
+struct sockaddr_in6 addr;
 
 MeshNode::MeshNode() {
   // Inicializar las constantes globales
   this->port = 8080;
   this->group = "ff02::1";
+  this->message = "Hola, desde el nodo en la red mesh!";
+  this->sockfd = socket(AF_INET6, SOCK_DGRAM, 0); // Crear el socket UDP IPv6
+
+  // Configurar dirección IPv6 y puerto del socket
+  this->configureSocket();
 }
 
 void MeshNode::startMeshNode() {
@@ -13,6 +25,30 @@ void MeshNode::startMeshNode() {
   printf("Estableciendo red mesh...\n\n");
   system("./scripts/setup-mesh-net.sh");
   printf("Red mesh establecida.\n\n");
+  printf("Presione Ctrl+C para detener la red mesh.\n");
+}
+
+void MeshNode::configureSocket() {
+  memset(&addr, 0, sizeof(addr));
+  addr.sin6_family = AF_INET6;
+  addr.sin6_port = htons(this->port);
+  inet_pton(AF_INET6, this->group.c_str(), &addr.sin6_addr);
+}
+
+void MeshNode::sendMessage() {
+  // Enviar mensaje a todos los nodos en la red mesh
+  printf("Enviando mensaje a todos los nodos en la red mesh...\n");
+  sendto(this->sockfd, this->message.c_str(), this->message.length(), 0, (struct sockaddr *)&addr, sizeof(addr));
+  printf("Mensaje enviado.\n");
+}
+
+void MeshNode::receiveMessage() {
+  char buffer[1024];
+  struct sockaddr_in6 src_addr;
+  socklen_t src_addr_len = sizeof(src_addr);
+  recvfrom(this->sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&src_addr, &src_addr_len);
+
+  printf("Mensaje recibido: %s", buffer);
 }
 
 void MeshNode::stopMeshNode() {
@@ -20,4 +56,11 @@ void MeshNode::stopMeshNode() {
   printf("\nDeteniendo red mesh...\n");
   system("./scripts/stop-mesh-net.sh");
   printf("\nRed mesh detenida.\n");
+}
+
+MeshNode::~MeshNode() {
+  // Cerrar el socket
+  close(this->sockfd);
+
+  printf("Puerto cerrado.\n");
 }
