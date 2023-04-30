cf883eb6bc6b772cb32dbdf6a06e9e62a7b63694
Gabriel Lemus <glemus.stuart@gmail.com>
2023-04-29 23:37:53 -0600

    Actualizar script

--- scripts/batman-bridge.sh
+++ scripts/batman-bridge.sh
@@ -1,94 +1,104 @@
-#!/bin/bash
+#!/bin/sh
 
-#if using a raspberry pi or other similar device running a linux distro 
-#make sure to resize image using sudo raspi-config
-#may require sudo sed -i -e 's/\r$//' FILENAME.bash (Replace FILENAME with whatever you called the file) to work
-#may need to chmod 777 the file prior to running
+# This script will automatically join a mesh network.
+# Modified script from: 
+# https://www.irif.univ-paris-diderot.fr/~jch/software/files/wifi-autoconf.sh
 
-#if using ubuntu uncomment the line bellow
-systemctl stop NetworkManager
+essid="sistemas-inalambricos"
+channel=1
+ipaddress=192.168.1.1
 
-# Connects and sets up the mesh-metwork
-echo " Now setting up and configuring Batman-adv Mesh "
-sudo ifconfig wlp2s0 down
-sudo ifconfig wlp2s0 mtu 1532
+die() {
+	echo "$@" >&2
+	exit 1
+}
 
-#sets bat0 to handle all routing
-echo bat0 > /sys/class/net/wlp2s0/batman_adv/mesh_iface
-echo bat0 > /sys/class/net/enp0s31f6/batman_adv/mesh_iface
+findcmd() {
+	type "$1" > /dev/null || \
+	    die "Couldn't find $1, please install ${2:-it} or fix your path."
+}
 
-read -p 'Please give your node a new hostname:' hostVariable
-hostname $hostVariable
-sudo echo -n $hostVariable > /etc/init.d/hostname.sh
-sudo /etc/init.d/hostname.sh start
+if [ "$(whoami)" != root ]; then
+	die "Sorry, you need to be root."
+fi
 
-#sets wifi configuration passphrase for wifi should be used by all nodes
-read -p 'Please give your mesh network an essid or enter essid you wish to join: ' essidVariable
-sudo iwconfig wlp2s0 mode ad-hoc essid $essidVariable ap 02:12:34:56:78:9A channel 1
-read -p 'Please give your wireless mesh network a password: ' wpaPassphrase
-echo $wpaPassphrase "$essidVariable" > /etc/wpa_supplicant.conf
-#Runs the wpa_supplicant daemon to apply changes
-sudo wpa_supplicant -B -i wlp2s0 -DWext -c /etc/wpa_supplicant.conf
+usage="Usage: $0 [-d debuglevel] [-r interface] interface ip-address/cidr"
+debuglevel=0
+routeinterface=
 
-# Starts BATMAN-adv
-sudo modprobe batman-adv
+while getopts "Nd:r:" name
+do
+	case $name in
+		d) debuglevel="$OPTARG";;
+		r) routeinterface="$OPTARG";;
+		?) die "$usage"
+	esac
+done
 
-#Asks for input to assign ip, this ip is used to assign to your bridge node eg 192.168.1.1 
-read -p 'Please input the ip you want to use for bridge node eg 192.168.1.1: ' ipVariable
-echo "Thank you $ipVariable will now be used TAKE NOTE OF THIS"
-ip_command=`sudo ifconfig bat0 $ipVariable`
-sudo ifconfig bat0 echo $ipVariable
+shift $(($OPTIND - 1))
 
-#comment out or delete the one above and uncomment the line bellow to allow bat0 to assign ip automatically this is probably easier
-#sudo dhclient bridge-link
+[ $# -lt 2 ] && die "$usage"
 
-sudo brctl addbr bridge-link
-sudo brctl stp bridge-link on
-sudo brctl addif bridge-link bat0
-sudo brctl addif bridge-link enp0s31f6
-#set bridge loop avoidance
-sudo batctl bl 1
+findcmd iwconfig "wireless-tools"
+findcmd babeld
+findcmd generate-ipv6-address
 
-#removes ip addresses previously assigned to these devices as this is now managed by batman-adv
-sudo ifconfig enp0s31f6 0.0.0.0
-sudo ifconfig wlp2s0 0.0.0.0
+(ip -6 addr show dev lo | grep -q 'inet6') || \
+	die "No IPv6 address on lo, please modprobe ipv6"
 
-#Bring up bat0 interface
-ifconfig bat0 up
+interface="$1"
 
-# # Installs cronjob and runs daily checking for updates for debian/raspbian OS
-# # to keep it upto date for security updates only, should not break anything when it updates
-# echo " Now Installing CronJob and Setting daily checks for security Updates ONLY Will not update Core or Programs "
-# sudo aptitude install cron-apt anacron -y
-# sudo rm /etc/cron.d/cron-apt
-# cd /etc/cron.daily
-# sudo ln -s /usr/sbin/cron-apt
-# APTCOMMAND=/usr/bin/aptitude
-# cd /etc/cron-apt/action.d
-# sudo rm 3-download
-# sudo aptitude safe-upgrade -y quiet=2
+# order is important for mac80211-based drivers
 
-# # Set the batman_adv module to auto start on reboot
-# echo " Setting BATMAN-adv to auto start on reboot "
-# sudo sh -c "echo batman_adv >> /etc/modules"
-# sudo sh -c "echo batman_adv >> /etc/rc.local"
+ifconfig "$interface" down || die "Couldn't configure interface"
+iwconfig "$interface" mode ad-hoc || die "Couldn't configure interface"
+ifconfig "$interface" up || die "Couldn't configure interface"
+iwconfig "$interface" essid "$essid" channel $channel || \
+	die "Couldn't configure interface"
 
-echo " SETUP COMPLETE NOW DISPLAYING INFO FOR DEBUGGING "
+ip link set up dev "$interface" || die "Couldn't up interface"
 
-# Displays ip for debugging purposes
-ifconfig | grep Bcast
+shift
 
-# Shows nodes on the mesh will be empy if you have not set a node yet
-sudo batctl o
+ipaddress="$1"
 
-echo "Holy scripts Batman that was easier than I thought - Robin"
+ip addr add $ipaddress dev $interface || die "Couldn't set IPv4 address"
+ip -6 addr add $(generate-ipv6-address fe80::) dev $interface || die "Couldn't set IPv6(loopback) address"
 
-# References
-# https://sudoroom.org/wiki/Mesh/Server_security
-# https://jshi-lab.assembla.com/spaces/social-sight/wiki/ODROID_XU3_Setup?version=9
-# http://ascii.co.uk/art/batman
-# https://downloads.open-mesh.org/batman/manpages/batctl.8.html
-# https://www.thinkpenguin.com/gnu-linux/how-configure-wifi-card-using-command-line-or-terminal
-# http://pastebin.com/4U9vdFFm
-# https://www.open-mesh.org/projects/batman-adv/wiki/Tweaking
-# http://pastebin.com/eeTmL5XL
+if [ -n "$routeinterface" ]
+then
+	echo "setting up routing for interface $routeinterface"
+
+	iptables -t nat -A POSTROUTING -o $routeinterface -j MASQUERADE
+	iptables -A FORWARD -i $routeinterface -o $interface -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+	iptables -A FORWARD -i $interface -o $routeinterface -j ACCEPT
+
+	gateway=$(route -n | grep eth0 | awk '{print $2}' | head -1)
+	ip route add 0.0.0.0/0 via $gateway dev $routeinterface proto static
+fi
+
+terminate() {
+	echo -n 'Killing babel...'
+	[ -e /var/run/babeld.pid ] && kill "$(cat /var/run/babeld.pid)"
+	echo 'done.'
+	ip addr flush dev $interface
+	trap - EXIT
+	exit 0
+}
+
+trap terminate INT QUIT TERM EXIT
+
+# allow time for the link layer to associate
+sleep 1
+
+if [ -n "$routeinterface" ]
+then
+	babeld ${debuglevel:+-d} $debuglevel -z 3 -C 'redistribute metric 128' $interface &
+else
+	babeld ${debuglevel:+-d} $debuglevel -z 3 $interface &
+fi
+
+echo "Babeld running on $interface, press ^C to terminate."
+echo "IP-Address is: $ipaddress"
+
+while :; do sleep 3600; done
